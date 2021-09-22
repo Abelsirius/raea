cfbac85b08618a23eb0bb5ce35f9d58c36972727
Sirius <abelgm.crack@gmail.com>
2021-09-21 21:24:05 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import logo from './logo.svg';
 import './App.css';
-import Inicio from './Inicio';
 import Home from './Home';
 import Create from './Create'
 import Agregar  from './Agregar';
@@ -14,7 +13,7 @@ function App() {
   return ( 
   <BrowserRouter>
    <Switch>
-     <Route path="/Resultados" nombreClient={nClient}> 
+     <Route path="/Resultados"> 
         <Resultados />
       </Route> 
      <Route path="/Gestion"> 
